66dff0975323ac639adebdf6d38e9d809fdc9721
Kevin Bett <kevin@rebelcolony.com>
2022-09-08 21:22:21 +0100

    working

--- event.rb
+++ event.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # Your goal is to write a class which can be used to manage events and their handlers.
 # The class should work like this:
 #  * it has a .subscribe() method, which takes a block and stores it as a handler
@@ -10,13 +12,31 @@
 #  the context of handlers' execution is not important
 #  each handler will be subscribed at most once at any given moment of time. It can still be unsubscribed and then subscribed again
 class Event
+
+	attr_accessor :handlers
+
+	def initialize
+		@handlers = []
+	end
+
 	def subscribe(&block)
-		yield if block_given?
+		@handlers << block
 	end
 
 	def unsubscribe(&block)
+		tester = 1
+		@handlers.each do |h|
+			@handlers.delete(h) if h.call(tester) == block.call(tester)
+		end
 	end
 
 	def broadcast(*args)
+		result = []
+		@handlers.each do |h|
+			args.each do |a|
+				result << h.call(a)
+			end
+		end
+		result
 	end
 end
--- event_spec.rb
+++ event_spec.rb
@@ -1,6 +1,73 @@
-describe "Subscribe" do
-	it "should take a block and stores it" do
-		event = Event.new
-		event.subscribe { puts "hello from inside the block" }
+require './event.rb'
+require 'pry'
+
+describe Event do
+	before do
+		@event = Event.new
+	end
+
+	it "should have a handlers array" do
+		expect(@event.handlers).to eq([])
+	end
+
+	context "Subscribe:" do
+		it "should take a block & add to the handlers array" do
+			@event.subscribe { puts "hello from inside the block" }
+
+			expect(@event.handlers.length).to eq(1)
+		end
+
+		it "create a proc object" do
+			@event.subscribe { puts "hello from inside the block" }
+
+			expect(@event.handlers.first.class).to eq(Proc)
+		end
+
+		it "can add multiple blocks" do
+			@event.subscribe { |x| x * 1 } 
+			@event.subscribe { |x| x * 2 } 
+			@event.subscribe { |x| x * 2 } 
+
+			expect(@event.handlers.length).to eq(3)
+		end
+	end
+
+	context "Unusbscribe:" do
+		it "should take a block & remove from the handlers array" do
+			@event.subscribe { |x| x * 1 } 
+			@event.subscribe { |x| x * 2 } 
+
+			@event.unsubscribe { |x| x * 1 }
+
+			expect(@event.handlers.length).to eq(1)
+		end
+	end
+
+	context "Broadcast:" do
+		it "calls a block with an argument" do
+			@event.subscribe { |x| x * 1 }
+
+			expect(@event.broadcast(1)).to eq([1])
+		end
+
+		it "calls a block with arguments" do
+			@event.subscribe { |x| x * 1 }
+
+			expect(@event.broadcast(1,2)).to eq([1, 2])
+		end
+
+		it "calls blocks with an argument" do
+			@event.subscribe { |x| x * 1 }
+			@event.subscribe { |x| x * 2 }
+
+			expect(@event.broadcast(1)).to eq([1, 2])
+		end
+
+		it "calls blocks with arguments" do
+			@event.subscribe { |x| x * 1 }
+			@event.subscribe { |x| x * 2 }
+
+			expect(@event.broadcast(1,2)).to eq([1, 2, 2, 4])
+		end
 	end
 end
